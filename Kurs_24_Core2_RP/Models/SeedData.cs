d9c888df4dbe8122ba6f712e761ea3b0dcbe1f67
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-04-27 11:10:27 +0200

    d8

---
+++ Kurs_24_Core2_RP/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kurs_24_Core2_RP.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Kurs_24_Core2_RPContext(
+                serviceProvider
+                    .GetRequiredService<DbContextOptions<Kurs_24_Core2_RPContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "ABC filmowania nocą",
+                        ReleaseDate = DateTime.Parse("1999-12-24"),
+                        Genre = "Obyczajowy",
+                        Price = 12.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Rybacy na Wakacjach",
+                        ReleaseDate = DateTime.Parse("1978-4-12"),
+                        Genre = "Komedia",
+                        Price = 23.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Tomcio Paluch",
+                        ReleaseDate = DateTime.Parse("2005-8-21"),
+                        Genre = "Obyczajowy",
+                        Price = 34.69M
+                    },
+                    new Movie
+                    {
+                        Title = "Robaki nie płaczą",
+                        ReleaseDate = DateTime.Parse("2001-3-12"),
+                        Genre = "Komedia",
+                        Price = 16.19M
+                    },
+                    new Movie
+                    {
+                        Title = "Franek w okopie",
+                        ReleaseDate = DateTime.Parse("1967-11-11"),
+                        Genre = "Wojenny",
+                        Price = 56.69M
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Kurs_24_Core2_RP/Program.cs
+++ Kurs_24_Core2_RP/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Kurs_24_Core2_RP.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Kurs_24_Core2_RP
@@ -14,7 +17,23 @@ namespace Kurs_24_Core2_RP
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<Kurs_24_Core2_RPContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"Błąd podczas seedowania danych do bazy");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
